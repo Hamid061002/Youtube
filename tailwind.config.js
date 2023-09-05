8d3a7072b2f74087d089f2fe5271781d59e1635e
HamiD <clashroyale13501350@gmail.com>
2023-09-05 16:43:27 +0330

    left section of header created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'hover-small-icon': 'rgba(255, 255, 255, 0.2);'
+        ,'hover-down-item': '#616161'
+      }
+      
+    },
   },
   plugins: [],
 }
