78c632a26be6b249b7d05511ce79081bdc243b1c
HamiD <clashroyale13501350@gmail.com>
2023-09-06 23:51:48 +0330

    full header responsive <md

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,20 @@ module.exports = {
         'border': 'hsl(0, 0%, 18.82%);',
         'first-color':'#888784',
         'second-color':'#222222',
-        'body-bg-color': '#0F0F0F'
+        'body-bg-color': '#0F0F0F',
+        'hover-left-icon': '#343434',
+        'first-item': '#F1F1F1'
       },
       width: {
         'logo': '90px',
-        'width-lg-search': '595px'
+        'width-lg-search': '600px',
+        'width-518': '518px',
+      },
+      screens:{
+        lg2:'1150px',
+        w430: '430px',
+        w560: '560px',
+        w700: '700px'
       }
     },
   },
