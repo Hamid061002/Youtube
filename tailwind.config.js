514f0971ea61261b901620f8d3014ef2ca94d494
HamiD <clashroyale13501350@gmail.com>
2023-09-07 16:05:11 +0330

    navbar right section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,31 @@ module.exports = {
         'hover-small-icon': 'rgba(255, 255, 255, 0.2);',
         'hover-down-item': '#616161',
         'border': 'hsl(0, 0%, 18.82%);',
-        'first-color':'#888784',
-        'second-color':'#222222',
+        'first-color': '#888784',
+        'second-color': '#222222',
         'body-bg-color': '#0F0F0F',
         'hover-left-icon': '#343434',
-        'first-item': '#F1F1F1'
+        'first-item': '#F1F1F1',
+        'navbar': '#272727'
       },
       width: {
         'logo': '90px',
+        'width-72': '72px',
         'width-lg-search': '600px',
         'width-518': '518px',
       },
-      screens:{
-        lg2:'1150px',
+      height:{
+        74:'74px'
+      },
+      screens: {
+        lg2: '1150px',
         w430: '430px',
         w560: '560px',
-        w700: '700px'
+        w700: '700px',
+        w800: '800px'
+      },
+      fontSize: {
+        10: '10px'
       }
     },
   },
